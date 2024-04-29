274470a95043a4b13e6941143b68e5914a0286d9
SukiSubramaniamM <162970547+SukiSubramaniamM@users.noreply.github.com>
2024-04-29 05:35:33 -0200

    Add files via upload

---
+++ MIT_3/MIT_3/MIT_3/script.js
@@ -0,0 +1,10 @@
+
+
+  
+function check(){
+  var fname = document.getElementById("fname").value
+  var first = document.getElementById("fbox")
+  if(fname == " "){
+    fbox.innerHTML = "fname cannot be empty"
+  }
+}
